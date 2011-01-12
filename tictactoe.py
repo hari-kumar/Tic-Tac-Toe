839147defe508f444f2eecac82cc5ac4e67f94c2
Hari Kumar <hariprasad@gatech.edu>
2011-01-12 12:35:30 -0500

    added movePlayer method. added new attributes to specify computer and player symbols. corrected a typo in isValidMove method name

--- tictactoe.py
+++ tictactoe.py
@@ -3,7 +3,8 @@ class TicTacToe:
         self.grid = ['-','-','-','-','-','-','-','-','-']
         self.available_blocks = [1,2,3,4,5,6,7,8,9]
         self.winning_combos = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
-        self.fp_symbol = 'X'
+        self.player_symbol = 'X'
+        self.computer_symbol = '0'
         self.dimension = 3
 
     def display(self):
@@ -19,15 +20,30 @@ class TicTacToe:
             increment+=1 
         sys.stdout.write("\n")
 
-    def issValidMove(self, block):
+    def isValidMove(self, block):
         if block >9 or block < 1:
             return False
         if self.grid[block-1] == "-":
                 return True
         return False
 
+    def isEmptyGrid(self):
+        try:
+            # If any "-" exists in the grid, it is not empty
+            temp = self.grid.index("-")
+            return False
+        except:
+            return True
 
+    def movePlayer(self,block):
+        """ Returns True if the move was successful, else False"""
+        if self.isValidMove(block):
+            self.grid[block-1] = self.player_symbol
+            return True
+        return False
+            
 
 
 t = TicTacToe()
+t.movePlayer(2)
 t.display()
