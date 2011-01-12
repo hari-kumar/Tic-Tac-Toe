cc3f53f831ab6e0d205198e30370619e512f8ca1
Hari Kumar <hariprasad@gatech.edu>
2011-01-12 11:22:28 -0500

    Added display method. New class attr: dimension

--- tictactoe.py
+++ tictactoe.py
@@ -4,5 +4,20 @@ class TicTacToe:
         self.available_blocks = [1,2,3,4,5,6,7,8,9]
         self.winning_combos = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
         self.fp_symbol = 'X'
-        
+        self.dimension = 3
 
+    def display(self):
+        import sys
+        increment = 0
+        while increment < 3:
+            for eno, element in enumerate(self.grid[self.dimension*increment:self.dimension*increment+3]):
+                sys.stdout.write(element)
+                if eno < 2:
+                    sys.stdout.write(" | ")
+            if increment <2:
+                sys.stdout.write('\n--|---|--\n')
+            increment+=1 
+        sys.stdout.write("\n")
+
+t = TicTacToe()
+t.display()
